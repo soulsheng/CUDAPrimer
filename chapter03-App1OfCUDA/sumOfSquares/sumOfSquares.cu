cca1775145cfa3d8c42600bbfd74dba20696bf16
study <1806597655@qq.com>
2012-09-13 12:28:38 +0800

    估算gflops 每秒浮点运算次数， 选择gflops最大的cuda设备 gts 250 ： 207.36 gflops

--- chapter03-App1OfCUDA/sumOfSquares/sumOfSquares.cu
+++ chapter03-App1OfCUDA/sumOfSquares/sumOfSquares.cu
@@ -24,6 +24,13 @@ using namespace std;
 
 bool g_bQATest = false;
 
+#ifndef MIN
+#define MIN(a,b) ((a < b) ? a : b)
+#endif
+#ifndef MAX
+#define MAX(a,b) ((a > b) ? a : b)
+#endif
+
 #ifdef _WIN32
    #define STRCASECMP  _stricmp
    #define STRNCASECMP _strnicmp
@@ -66,7 +73,97 @@ void processArgs(int argc, char **argv)
         }
     }
 }
+// Beginning of GPU Architecture definitions
+inline int _ConvertSMVer2Cores(int major, int minor)
+{
+    // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+    typedef struct {
+       int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+       int Cores;
+    } sSMtoCores;
 
+	sSMtoCores nGpuArchCoresPerSM[] = 
+	{ { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
+	  { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
+	  { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
+	  { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
+	  { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+	  { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+	  { 0x30, 192}, // Fermi Generation (SM 3.0) GK10x class
+	  {   -1, -1 }
+	};
+
+    int index = 0;
+    while (nGpuArchCoresPerSM[index].SM != -1) {
+       if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+          return nGpuArchCoresPerSM[index].Cores;
+       }	
+       index++;
+    }
+    printf("MapSMtoCores undefined SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
+    return -1;
+}
+// 硬件拥有最大的浮点计算能力GFLOPS
+int gpuGetMaxGflopsDeviceId(float& fGFLOPS)
+{
+    int current_device     = 0, sm_per_multiproc  = 0;
+    int max_compute_perf   = 0, max_perf_device   = 0;
+    int device_count       = 0, best_SM_arch      = 0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceCount( &device_count );
+    
+    // Find the best major SM Architecture GPU device
+    while (current_device < device_count)
+    {
+        cudaGetDeviceProperties( &deviceProp, current_device );
+        if (deviceProp.major > 0 && deviceProp.major < 9999)
+        {
+            best_SM_arch = MAX(best_SM_arch, deviceProp.major);
+        }
+        current_device++;
+    }
+
+    // Find the best CUDA capable GPU device
+    current_device = 0;
+	while( current_device < device_count )
+	{
+		cudaGetDeviceProperties( &deviceProp, current_device );
+		if (deviceProp.major == 9999 && deviceProp.minor == 9999)
+		{
+			sm_per_multiproc = 1;
+		}
+		else
+		{
+			sm_per_multiproc = _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
+		}
+
+		int compute_perf  = deviceProp.multiProcessorCount * sm_per_multiproc * deviceProp.clockRate;
+		// clockRate指shader的频率，单位是kHz，即"Clock frequency in kilohertz "，参考：http://developer.download.nvidia.com/compute/cuda/4_2/rel/toolkit/docs/online/structcudaDeviceProp_dee14230e417cb3059d697d6804da414.html#dee14230e417cb3059d697d6804da414
+
+		if( compute_perf  > max_compute_perf )
+		{
+			// If we find GPU with SM major > 2, search only these
+			if ( best_SM_arch > 2 )
+			{
+				// If our device==dest_SM_arch, choose this, or else pass
+				if (deviceProp.major == best_SM_arch)
+				{
+					max_compute_perf  = compute_perf;
+					max_perf_device   = current_device;
+				}
+			}
+			else
+			{
+				max_compute_perf  = compute_perf;
+				max_perf_device   = current_device;
+				printf("sp核数：%d, shader频率: %d \n", deviceProp.multiProcessorCount * sm_per_multiproc, deviceProp.clockRate);
+			}
+		}
+		++current_device;
+	}
+	fGFLOPS = max_compute_perf * 1.0e-6;
+	return max_perf_device;
+}
 bool InitCUDA()
 {
     int count;
@@ -78,6 +175,12 @@ bool InitCUDA()
     }
 
     int i;
+#if 1
+	float fGFLOPS = 0.0f;
+	i = gpuGetMaxGflopsDeviceId( fGFLOPS );
+    printf("计算能力估算公式=sp核数 * shader频率 \n\
+			计算能力粗略估算: %0.2f GFLOPS\n", fGFLOPS);
+#else
     for(i = 0; i < count; i++) {
         cudaDeviceProp prop;
         if(cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
@@ -86,7 +189,7 @@ bool InitCUDA()
             }
         }
     }
-
+#endif
     if(i == count) {
         fprintf(stderr, "There is no device supporting CUDA 1.x.\n");
         return false;
