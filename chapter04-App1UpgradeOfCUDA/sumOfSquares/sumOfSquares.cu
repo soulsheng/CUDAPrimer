6a6dee2cb71f96bb2c420ebe3931fbf3b1432000
study <1806597655@qq.com>
2012-09-13 16:27:09 +0800

    第四章 沿用第三章的源码 直接拷贝

---
+++ chapter04-App1UpgradeOfCUDA/sumOfSquares/sumOfSquares.cu
@@ -0,0 +1,311 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+/* Template project which demonstrates the basics on how to setup a project 
+* example application, doesn't use cutil library.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+//#include <shrQATest.h>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+bool g_bQATest = false;
+
+#ifndef MIN
+#define MIN(a,b) ((a < b) ? a : b)
+#endif
+#ifndef MAX
+#define MAX(a,b) ((a > b) ? a : b)
+#endif
+
+#ifdef _WIN32
+   #define STRCASECMP  _stricmp
+   #define STRNCASECMP _strnicmp
+#else
+   #define STRCASECMP  strcasecmp
+   #define STRNCASECMP strncasecmp
+#endif
+
+#define ASSERT(x, msg, retcode) \
+    if (!(x)) \
+    { \
+        cout << msg << " " << __FILE__ << ":" << __LINE__ << endl; \
+        return retcode; \
+    }
+
+__global__ void sequence_gpu(int *d_ptr, int length)
+{
+    int elemID = blockIdx.x * blockDim.x + threadIdx.x;
+    if (elemID < length)
+    {
+        d_ptr[elemID] = elemID;
+    }
+}
+
+
+void sequence_cpu(int *h_ptr, int length)
+{
+    for (int elemID=0; elemID<length; elemID++)
+    {
+        h_ptr[elemID] = elemID;
+    }
+}
+
+void processArgs(int argc, char **argv)
+{
+    for (int i=1; i < argc; i++) {
+        if((!STRNCASECMP((argv[i]+1), "noprompt", 8)) || (!STRNCASECMP((argv[i]+2), "noprompt", 8)) )
+        {
+            g_bQATest = true;
+        }
+    }
+}
+// Beginning of GPU Architecture definitions
+inline int _ConvertSMVer2Cores(int major, int minor)
+{
+    // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+    typedef struct {
+       int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+       int Cores;
+    } sSMtoCores;
+
+	sSMtoCores nGpuArchCoresPerSM[] = 
+	{ { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
+	  { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
+	  { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
+	  { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
+	  { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+	  { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+	  { 0x30, 192}, // Fermi Generation (SM 3.0) GK10x class
+	  {   -1, -1 }
+	};
+
+    int index = 0;
+    while (nGpuArchCoresPerSM[index].SM != -1) {
+       if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+          return nGpuArchCoresPerSM[index].Cores;
+       }	
+       index++;
+    }
+    printf("MapSMtoCores undefined SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
+    return -1;
+}
+// 硬件拥有最大的浮点计算能力GFLOPS
+int gpuGetMaxGflopsDeviceId(float& fGFLOPS)
+{
+    int current_device     = 0, sm_per_multiproc  = 0;
+    int max_compute_perf   = 0, max_perf_device   = 0;
+    int device_count       = 0, best_SM_arch      = 0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceCount( &device_count );
+    
+    // Find the best major SM Architecture GPU device
+    while (current_device < device_count)
+    {
+        cudaGetDeviceProperties( &deviceProp, current_device );
+        if (deviceProp.major > 0 && deviceProp.major < 9999)
+        {
+            best_SM_arch = MAX(best_SM_arch, deviceProp.major);
+        }
+        current_device++;
+    }
+
+    // Find the best CUDA capable GPU device
+    current_device = 0;
+	while( current_device < device_count )
+	{
+		cudaGetDeviceProperties( &deviceProp, current_device );
+		if (deviceProp.major == 9999 && deviceProp.minor == 9999)
+		{
+			sm_per_multiproc = 1;
+		}
+		else
+		{
+			sm_per_multiproc = _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
+		}
+
+		int compute_perf  = deviceProp.multiProcessorCount * sm_per_multiproc * deviceProp.clockRate;
+		// clockRate指shader的频率，单位是kHz，即"Clock frequency in kilohertz "，参考：http://developer.download.nvidia.com/compute/cuda/4_2/rel/toolkit/docs/online/structcudaDeviceProp_dee14230e417cb3059d697d6804da414.html#dee14230e417cb3059d697d6804da414
+
+		if( compute_perf  > max_compute_perf )
+		{
+			// If we find GPU with SM major > 2, search only these
+			if ( best_SM_arch > 2 )
+			{
+				// If our device==dest_SM_arch, choose this, or else pass
+				if (deviceProp.major == best_SM_arch)
+				{
+					max_compute_perf  = compute_perf;
+					max_perf_device   = current_device;
+				}
+			}
+			else
+			{
+				max_compute_perf  = compute_perf;
+				max_perf_device   = current_device;
+				printf("sp核数：%d, shader频率: %d \n", deviceProp.multiProcessorCount * sm_per_multiproc, deviceProp.clockRate);
+			}
+		}
+		++current_device;
+	}
+	fGFLOPS = max_compute_perf * 1.0e-6;
+	return max_perf_device;
+}
+bool InitCUDA()
+{
+    int count;
+
+    cudaGetDeviceCount(&count);
+    if(count == 0) {
+        fprintf(stderr, "There is no device.\n");
+        return false;
+    }
+
+    int i;
+#if 1
+	float fGFLOPS = 0.0f;
+	i = gpuGetMaxGflopsDeviceId( fGFLOPS );
+    printf("计算能力估算公式=sp核数 * shader频率 \n\
+			计算能力粗略估算: %0.2f GFLOPS\n", fGFLOPS);
+#else
+    for(i = 0; i < count; i++) {
+        cudaDeviceProp prop;
+        if(cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
+            if(prop.major >= 1) {
+                break;
+            }
+        }
+    }
+#endif
+    if(i == count) {
+        fprintf(stderr, "There is no device supporting CUDA 1.x.\n");
+        return false;
+    }
+
+    cudaSetDevice(i);
+
+    return true;
+}
+
+#define DATA_SIZE (1<<20)//1048576
+
+int data[DATA_SIZE];
+
+void GenerateNumbers(int *number, int size)
+{
+    for(int i = 0; i < size; i++) {
+        number[i] = rand() % 10;
+    }
+}
+
+__global__ static void sumOfSquares(int *num, int* result, clock_t* time)
+{
+    int sum = 0;
+    int i;
+	clock_t start = clock();
+    for(i = 0; i < DATA_SIZE; i++) {
+        sum += num[i] * num[i];
+    }
+
+    *result = sum;
+    *time = clock() - start;
+}
+
+int main(int argc, char **argv)
+{
+	//shrQAStart(argc, argv);
+	 if(!InitCUDA()) {
+        return 0;
+    }
+
+    printf("CUDA initialized.\n");
+
+	 GenerateNumbers(data, DATA_SIZE);
+
+    int* gpudata, *result;
+	clock_t* time;
+    cudaMalloc((void**) &gpudata, sizeof(int) * DATA_SIZE);
+    cudaMalloc((void**) &result, sizeof(int));
+    cudaMalloc((void**) &time, sizeof(clock_t));
+    cudaMemcpy(gpudata, data, sizeof(int) * DATA_SIZE, cudaMemcpyHostToDevice);
+
+	sumOfSquares<<<1, 1, 0>>>(gpudata, result, time);
+
+    int sum;
+    clock_t time_used;
+    cudaMemcpy(&sum, result, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&time_used, time, sizeof(clock_t), cudaMemcpyDeviceToHost);
+    cudaFree(gpudata);
+    cudaFree(result);
+
+    printf("sum: %d\n", sum);
+
+	sum = 0;
+    for(int i = 0; i < DATA_SIZE; i++) {
+        sum += data[i] * data[i];
+    }
+    printf("sum: %d of %d squares\n", sum, DATA_SIZE);
+    printf("time: %d, time/n: %d\n", time_used, time_used/DATA_SIZE);
+
+#if 0
+    cout << "CUDA Runtime API template" << endl;
+    cout << "=========================" << endl;
+    cout << "Self-test started" << endl;
+
+    const int N = 100;
+
+    processArgs(argc, argv);
+
+    int *d_ptr;
+    ASSERT(cudaSuccess == cudaMalloc    (&d_ptr, N * sizeof(int)), "Device allocation of " << N << " ints failed", -1);
+
+    int *h_ptr;
+    ASSERT(cudaSuccess == cudaMallocHost(&h_ptr, N * sizeof(int)), "Host allocation of "   << N << " ints failed", -1);
+
+    cout << "Memory allocated successfully" << endl;
+
+    dim3 cudaBlockSize(32,1,1);
+    dim3 cudaGridSize((N + cudaBlockSize.x - 1) / cudaBlockSize.x, 1, 1);
+    sequence_gpu<<<cudaGridSize, cudaBlockSize>>>(d_ptr, N);
+    ASSERT(cudaSuccess == cudaGetLastError(), "Kernel launch failed", -1);
+    ASSERT(cudaSuccess == cudaDeviceSynchronize(), "Kernel synchronization failed", -1);
+
+    sequence_cpu(h_ptr, N);
+
+    cout << "CUDA and CPU algorithm implementations finished" << endl;
+
+    int *h_d_ptr;
+    ASSERT(cudaSuccess == cudaMallocHost(&h_d_ptr, N * sizeof(int)), "Host allocation of " << N << " ints failed", -1);
+    ASSERT(cudaSuccess == cudaMemcpy(h_d_ptr, d_ptr, N * sizeof(int), cudaMemcpyDeviceToHost), "Copy of " << N << " ints from device to host failed", -1);
+    bool bValid = true;
+    for (int i=0; i<N && bValid; i++)
+    {
+        if (h_ptr[i] != h_d_ptr[i])
+        {
+            bValid = false;
+        }
+    }
+
+    ASSERT(cudaSuccess == cudaFree(d_ptr),       "Device deallocation failed", -1);
+    ASSERT(cudaSuccess == cudaFreeHost(h_ptr),   "Host deallocation failed",   -1);
+    ASSERT(cudaSuccess == cudaFreeHost(h_d_ptr), "Host deallocation failed",   -1);
+
+    cout << "Memory deallocated successfully" << endl;
+    cout << "TEST Results " << endl;
+#endif  
+    //shrQAFinishExit(argc, (const char **)argv, (bValid ? QA_PASSED : QA_FAILED));
+	system("pause");
+}
